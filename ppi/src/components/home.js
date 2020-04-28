5c0762e82700c5811ca67c8b0e9701d3dd4da645
Ruth Nohemy Ardón Lechuga <ruthlechuga.1997@gmail.com>
2020-04-27 13:02:37 -1200

    visualizacion de sedes

--- ppi/src/components/home.js
+++ ppi/src/components/home.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link} from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import IconButton from '@material-ui/core/IconButton';
+import Sedes from "./sedes";
+import { BrowserRouter, Route } from 'react-router-dom'
 
 export default class Home extends Component {
     render() {
+        const style={
+            back:{
+                float:"right"
+            }
+        }
         return (
             <Router>
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                    <div className="container">
+                        <Button
+                            size="large"
+                            title="Home"
+                            onClick={() => {
+                                this.props.history.push("/vendedor_home")
+                            }}
+                        >Grupo 8</Button>
+                        <IconButton aria-label="delete" style={style.back} onClick={()=>{                         
+                        }}>
+                            <ArrowBackIcon fontSize="large" />
+                        </IconButton>
+                    </div>
+                </nav>
+                <br/>
+                <br/>
+                <br/>
+
                 <div className="container">
                 <br></br><br></br><br></br>
                 <div class="row">
@@ -13,7 +42,8 @@ export default class Home extends Component {
                         <div class="card-body">
                             <h5 className="card-title">Administrar sedes</h5>
                             <p className="card-text">Registrar, eliminar y modificar información de sedes existentes</p>
-                            <a href="#" class="btn btn-light"><Link to="sedes" className="link">Ver información</Link></a>
+                            
+                            <a href="sedes" class="btn btn-light">Ver información</a>
                         </div>
                         </div>
                     </div>
--- ppi/src/components/sedes.js
+++ ppi/src/components/sedes.js
@@ -1,35 +1,65 @@
 import React, { Component } from "react";
+import { forwardRef } from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 import axios from 'axios';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 export default class Sedes extends Component {
+
+    tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };
 
     constructor(props) {
         super(props)
 
         this.state = {
-           sedes: []
+            columns: [
+                { title: 'ID', field: 'id_sede' },
+                { title: 'ALIAS', field: 'alias' },
+                { title: 'DIRECCION', field: 'direccion'},
+            ],
+            data: []
         }
-     }
+    }
 
-    renderTableData() {
-
+    componentDidMount() {
         axios.get('https://proyectopi-server.herokuapp.com/sede')
         .then(res => {
-            console.log(res);
-            this.state.sedes = res.data;
+            const data = res.data;
+            this.setState({data});
         });
-
-        return this.state.sedes.map((sede, index) => {
-           const { id_sede, alias, direcccion } = sede //destructuring
-           return (
-              <tr class="table-light" key={id_sede}>
-                 <td>{id_sede}</td>
-                 <td>{alias}</td>
-                 <td>{direcccion}</td>
-              </tr>
-           )
-        })
     }
 
     render() {
@@ -38,18 +68,50 @@ export default class Sedes extends Component {
                 <br></br><br></br><br></br>
                 <div>
                     <h1 id='title'>Sede</h1>
-                    <table class="table" id='sedes'>
-                    <thead class="thead-dark">
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Alias</th>
-                        <th scope="col">Direccion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderTableData()}
-                    </tbody>
-                    </table>
+                    <br></br><br></br>
+                    <MaterialTable
+                        icons={this.tableIcons}
+                        title="Sedes existentes"
+                        columns={this.state.columns}
+                        data={this.state.data}
+                        editable={{
+                            onRowAdd: (newData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                resolve();
+                                this.setState((prevState) => {
+                                    const data = [...prevState.data];
+                                    data.push(newData);
+                                    return { ...prevState, data };
+                                });
+                                }, 600);
+                            }),
+                            onRowUpdate: (newData, oldData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                resolve();
+                                if (oldData) {
+                                    this.setState((prevState) => {
+                                    const data = [...prevState.data];
+                                    data[data.indexOf(oldData)] = newData;
+                                    return { ...prevState, data };
+                                    });
+                                }
+                                }, 600);
+                            }),
+                            onRowDelete: (oldData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                resolve();
+                                this.setState((prevState) => {
+                                    const data = [...prevState.data];
+                                    data.splice(data.indexOf(oldData), 1);
+                                    return { ...prevState, data };
+                                });
+                                }, 600);
+                            }),
+                        }}
+                    />
                 </div>
             </Router>
         );
